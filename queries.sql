57ef09bada75a87c5d9069f8ab2ce265c88aec6b
jmwatson2357 <54993615+jmwatson2357@users.noreply.github.com>
2021-04-02 13:27:49 -0500

    added SQL Part 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 ## Part 1: Test it with SQL
-DESCRIBE job;
+    DESCRIBE job;
 ## Part 2: Test it with SQL
-SELECT name FROM employers WHERE location="St. Louis City";
+    SELECT name FROM employers WHERE location="St. Louis City";
 ## Part 3: Test it with SQL
-DROP TABLE techjobs.job;
+    DROP TABLE techjobs.job;
 ## Part 4: Test it with SQL
+    SELECT skill.name, skill.description
+    FROM skill
+    LEFT JOIN job_skills on skill.id = job_skills.skills_id
+    WHERE job_skills.skills_id IS NOT NULL;
